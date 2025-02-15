b9ce0c3d5a8f969bc63656740d37b035945a9553
AkmalD <goniyyu@gmail.com>
2025-02-15 08:34:53 +0700

    Third Commit: Part 4

--- order-service/src/main/java/com/plbo/example/order/service/OrderService.java
+++ order-service/src/main/java/com/plbo/example/order/service/OrderService.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+
+import com.plbo.example.order.client.InventoryClient;
 import com.plbo.example.order.dto.OrderRequest;
 import com.plbo.example.order.model.Order;
 import com.plbo.example.order.repository.OrderRepository;
@@ -13,8 +15,25 @@ import com.plbo.example.order.repository.OrderRepository;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
 
     public void placeOrder(OrderRequest orderRequest){
+
+        // 1. Using Mockito
+        // 2. Using Wiremock
+        var isProductionInStock =  inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+
+        if(isProductionInStock){
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+            orderRepository.save(order);        
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is out of stock");
+        }
+        
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setPrice(orderRequest.price());
